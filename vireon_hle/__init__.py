e31ef0ba3365be59e48608f57b4d04564440933e
Inkwon Song Jr. <a3fpro@gmail.com>
2025-12-08 00:02:43 -0700

    Create __init__.py

---
+++ vireon_hle/__init__.py
@@ -0,0 +1,10 @@
+"""
+Vireon-HLE core package.
+
+HLE = Human-Level Evaluation:
+- Question sets
+- Model responses
+- Scoring & metrics
+"""
+
+from .engine import HLEEngine, HLEResult  # noqa: F401
